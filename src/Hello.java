59260dd4c0d427247fb2343add2e102374ee148d
Daniel Berryman <danielberryman90@gmail.com>
2019-05-25 21:52:25 -0400

    playing with ints

--- src/Hello.java
+++ src/Hello.java
@@ -1,9 +1,15 @@
 public class Hello {
-
     public static void main(String [] args) {
 
         System.out.println("Hello, World!");
+        int myFirstNumber = (15 + 10) + 15/3;
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
 
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int challenge = 1000 - myTotal;
+
+        System.out.println(challenge);
     }
-
 }
